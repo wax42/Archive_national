e995ab9e7c587c8b1f1cbcf0f434c1e5d249242b
wax42 <vguerand@student.42.fr>
2018-12-07 16:03:37 +0100

    Essaie

---
+++ push.sh
@@ -0,0 +1,1 @@
+npm run build;./node_modules/.bin/push-dir --dir=dist --branch=gh-pages --cleanup
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <div>
+    <input type="text" placeholder="Text input" @focus="show" data-layout="normal" />
+    <vue-touch-keyboard v-if="visible", :layout="layout", :cancel="hide", :accept="accept", :input="input" />
+    </div>
   </div>
 </template>
 
 <script>
+import VueTouchKeyboard from "vue-touch-keyboard";
+import Vue from 'vue'
+import style from "vue-touch-keyboard/dist/vue-touch-keyboard.css"; // load default style
+
+Vue.use(VueTouchKeyboard);
+
 export default {
-  name: 'App'
+  name: 'App',
+  data: {
+    visible: true,
+    layout: "normal",
+    options: {
+      useKbEvents: false,
+      preventClickEvent: false
+      }
+  },
+  methods: {
+    accept(text) {
+      alert("Input text: " + text);
+      this.hide();
+    },
+
+    show(e) {
+      this.input = e.target;
+      this.layout = e.target.dataset.layout;
+
+      if (!this.visible)
+        this.visible = true
+    },
+    hide() {
+      this.visible = false;
+    }
+  }
 }
 </script>
 
